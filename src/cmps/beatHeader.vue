cd8e762be7a5a0f976fec70533a531c38deeea69
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-11-25 22:10:42 +0200

    continue header

--- src/cmps/beatHeader.vue
+++ src/cmps/beatHeader.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="header flex justify-center align-center space-between">
         <img class="beat-logo" src="@/assets/img/beatLogo.png" />
-        <el-input type="text" placeholder="Serch for beat" prefix-icon="el-icon-search" v-model="genre" />
-        
-        <router-link to="/app">BEATS</router-link>
-        <router-link to="/app/add"> CREATE BEAT</router-link>
+        <el-input class="header-search" type="text" placeholder="Serch for beat" prefix-icon="el-icon-search" v-model="genre" />
+        <div class="flex justify-center align-center space-between">
+        <router-link class="header-btn" to="/app">BEATS ||</router-link>
+        <router-link class="header-btn" to="/app/add">  CREATE BEAT</router-link>
+        </div>
+        <div>
+              <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+        </div>
     </div>
 </template>
 
