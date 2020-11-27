29cacd02336586ee435c9439c647506834c952a5
omer <omerfussfeld@gmail.com>
2020-11-27 19:07:29 +0200

    aa

--- src/cmps/beatDetails/beatInfo.vue
+++ src/cmps/beatDetails/beatInfo.vue
@@ -7,16 +7,17 @@
     <div class="beat-desc">
       <h2>{{ beat.name }}</h2>
       <h3>{{ beat.genre }}</h3>
-      <h4>{{beat.description}}</h4>
+      <h4>{{ beat.description }}</h4>
     </div>
     <div class="views-container beat-desc">
       <div class="flex">
-          <i class="like-btn far fa-heart"></i>
+        <i class="like-btn far fa-heart"></i>
         <h5>{{ beat.likes }}</h5>
       </div>
       <h5>VISITS: {{ beat.visits }}</h5>
       <h5>ONLINE: 2</h5>
     </div>
+    <i @click="removeBeat(song.id)" class="remove-beat icon fas fa-trash"></i>
   </section>
 </template>
 
