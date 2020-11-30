99aaf5d383a2656a99f96ecf90158173fcc09e58
omer <omerfussfeld@gmail.com>
2020-11-30 13:14:33 +0200

    A

--- src/cmps/beatDetails/addSong.vue
+++ src/cmps/beatDetails/addSong.vue
@@ -3,10 +3,10 @@
     <div v-for="song in searchedSongs" :key="song.id">
       <div class="song-container flex align-center space-between icon">
         <img class="song-img" :src="song.imgUrl" />
-        <h4 class="song-title">{{ song.title }}</h4>
+        <h5 class="song-title">{{ song.title }}</h5>
         <div class="flex">
           <h5 class="song-dur">{{ song.duration }}</h5>
-          <i class="fas fa-plus" @click="addSongToPlayList(song)"></i>
+          <i class="song-icon fas fa-plus" @click="addSongToPlayList(song)"></i>
         </div>
       </div>
     </div>
--- src/cmps/beatPreview.vue
+++ src/cmps/beatPreview.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="prev-card" @click="beatPrev(beat._id)">
     <div class="beat-img flex align-center justify-center">
-    <img class="prev-img" :src="beat.imgUrl" />
+      <img class="prev-img" :src="beat.imgUrl" />
     </div>
     <div class="prev-details flex col align-center">
       <h2 class="prev-name">{{ beat.name }}</h2>
       <h3 class="prev-genre">{{ beat.genre }}</h3>
       <div class="prev-info-container flex space-around align-center">
-       
-         <span class="prev-info">  <i class="prev-likes prev- fas fa-heart"></i> {{ beat.likes }} </span>
+        <span class="prev-info">
+          <i class="prev-likes prev- fas fa-heart"></i> {{ beat.likes }}
+        </span>
         <span class="prev-info">25 Online</span>
         <span class="prev-info">{{ beat.songs.length }} Songs</span>
       </div>
