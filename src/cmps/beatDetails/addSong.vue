15a0430a08e6fe624ad55e0364d8768d5b538957
omer <omerfussfeld@gmail.com>
2020-11-30 12:57:47 +0200

    a

--- src/cmps/beatDetails/addSong.vue
+++ src/cmps/beatDetails/addSong.vue
@@ -1,24 +1,15 @@
 <template>
-  <section class="beat-add">
-    <input
-      type="text"
-      class="send-msg"
-      @keyup="searchYoutubeSong"
-      v-model="searchKeyWord"
-      placeholder="Search song"
-    />
-    <section v-if="searchedSongs" class="add-container">
-      <div v-for="song in searchedSongs" :key="song.id">
-        <div class="song-container flex align-center space-between icon">
-          <img class="song-img" :src="song.imgUrl" />
-          <h4 class="song-title">{{ song.title }}</h4>
-          <div class="flex">
-            <h5 class="song-dur">{{ song.duration }}</h5>
-            <i class="fas fa-plus" @click="addSongToPlayList(song)"></i>
-          </div>
+  <section v-if="searchedSongs" class="beat-add add-container">
+    <div v-for="song in searchedSongs" :key="song.id">
+      <div class="song-container flex align-center space-between icon">
+        <img class="song-img" :src="song.imgUrl" />
+        <h4 class="song-title">{{ song.title }}</h4>
+        <div class="flex">
+          <h5 class="song-dur">{{ song.duration }}</h5>
+          <i class="fas fa-plus" @click="addSongToPlayList(song)"></i>
         </div>
       </div>
-    </section>
+    </div>
   </section>
 </template>
 
@@ -29,15 +20,10 @@ export default {
     searchedSongs: Array,
   },
   data() {
-    return {
-      searchKeyWord: "",
-    };
+    return {};
   },
   computed: {},
   methods: {
-    async searchYoutubeSong() {
-      this.$emit("setKeyWord", this.searchKeyWord);
-    },
     addSongToPlayList(song) {
       this.$emit("addSongToPlayList", song);
     },
--- src/views/beatApp.vue
+++ src/views/beatApp.vue
@@ -1,43 +1,43 @@
 <template>
-    <section class="main-layout">
-        <genre-filter  @setGenre="setGenreFilter" class="flex align-center justify-center"></genre-filter>
-        <beat-list :beats="beats" />
-    </section>
+  <section class="main-layout">
+    <genre-filter
+      @setGenre="setGenreFilter"
+      class="flex align-center justify-center"
+    ></genre-filter>
+    <beat-list :beats="beats" />
+  </section>
 </template>
 
 <script>
 import beatList from "@/cmps/beatList.vue";
 import genreFilter from "@/cmps/genreFilter.vue";
 export default {
-    name: "beat",
-    data() {
-        return {
-         
-        };
+  name: "beat",
+  data() {
+    return {};
+  },
+  methods: {
+    setGenreFilter(genre) {
+      this.$store.dispatch({
+        type: "setGenreFilter",
+        selectedGenre: genre,
+      });
     },
-    methods: {
-     setGenreFilter(genre){
-        this.$store.dispatch({
-                type: "setGenreFilter",
-                selectedGenre: genre
-            })
-     }
+  },
+  components: {
+    beatList,
+    genreFilter,
+  },
+  computed: {
+    beats() {
+      return this.$store.getters.beats;
     },
-    components: {
-        beatList,
-        genreFilter,
+    genreFilter() {
+      return this.$store.getters.genreFilter;
     },
-    computed: {
-        beats() {
-            return this.$store.getters.beats;
-        },
-        genreFilter() {
-            return this.$store.getters.genreFilter;
-        },
-  
-    },
-    created() {
-        this.$store.dispatch({ type: "loadBeats" });
-    },
+  },
+  created() {
+    this.$store.dispatch({ type: "loadBeats" });
+  },
 };
 </script>
