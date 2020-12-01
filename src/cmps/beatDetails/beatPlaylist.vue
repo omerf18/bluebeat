1b400bc36f1e697eb12a0697f04be19713a9a2ad
omer <omerfussfeld@gmail.com>
2020-12-01 13:35:18 +0200

    aa

--- src/cmps/beatDetails/beatPlaylist.vue
+++ src/cmps/beatDetails/beatPlaylist.vue
@@ -1,27 +1,33 @@
 <template>
   <section>
-    <div v-for="(song, index) in playlist" :key="song.id">
-      <div
-        class="song-container flex space-between align-center icon"
-        :class="{ active: index === currSongIdx }"
-        @click="changeSong(song)"
-      >
-        <img class="song-img" :src="song.imgUrl" />
-        <h5 class="song-title">{{ song.title }}</h5>
-        <div class="flex">
-          <h5 class="song-dur">{{ song.duration }}</h5>
-          <i
-            @click="removeSong(song.id)"
-            class="song-icon icon fas fa-trash"
-          ></i>
+    <draggable v-model="playlist" ghost-class="ghost" @end="onEnd">
+      <transition-group type="transition" name="flip-list">
+        <div
+          v-for="(song, index) in playlist"
+          :key="song.id"
+          class="song-container sortable flex space-between align-center icon"
+          :class="{ active: index === currSongIdx }"
+          :id="song.id"
+          @click="changeSong(song)"
+        >
+          <img class="song-img" :src="song.imgUrl" />
+          <h5 class="song-title">{{ song.title }}</h5>
+          <div class="flex">
+            <h5 class="song-dur">{{ song.duration }}</h5>
+            <i
+              @click="removeSong(song.id)"
+              class="song-icon icon fas fa-trash"
+            ></i>
+          </div>
         </div>
-      </div>
-    </div>
+      </transition-group>
+    </draggable>
   </section>
 </template>
 
 <script>
 import socketService from "../../services/socketService.js";
+import draggable from "vuedraggable";
 export default {
   props: {
     playlist: Array,
@@ -29,7 +35,10 @@ export default {
   },
   name: "playlist",
   data() {
-    return {};
+    return {
+      oldIndex: "",
+      newIndex: "",
+    };
   },
   computed: {
     serPlaylist() {
@@ -37,6 +46,11 @@ export default {
     },
   },
   methods: {
+    onEnd(ev) {
+      console.log("ev: ", ev);
+      this.oldIndex = ev.oldIndex;
+      this.newIndex = ev.newIndex;
+    },
     removeSong(songId) {
       this.$emit("removeSong", songId);
     },
@@ -45,5 +59,8 @@ export default {
     },
   },
   created() {},
+  components: {
+    draggable,
+  },
 };
 </script>
