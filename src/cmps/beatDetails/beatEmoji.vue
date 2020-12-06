1e50a38bbb8e69d43f57c549fe79121230d8d5cc
omer <omerfussfeld@gmail.com>
2020-12-06 19:36:50 +0200

    A

--- src/cmps/beatDetails/beatEmoji.vue
+++ src/cmps/beatDetails/beatEmoji.vue
@@ -21,96 +21,12 @@
 </template>
 <script>
 import { utilService } from "../../services/utilService.js";
+import { emojiService } from "../../services/emojiService.js";
 export default {
   name: "emojiPicker",
   data() {
     return {
-      emojis: [
-        {
-          id: 1,
-          class: "far fa-smile",
-        },
-        {
-          id: 2,
-          class: "far fa-smile-wink",
-        },
-        {
-          id: 3,
-          class: "far fa-grin-stars",
-        },
-        {
-          id: 4,
-          class: "far fa-grin-squint-tears",
-        },
-        {
-          id: 5,
-          class: "far fa-dizzy",
-        },
-        {
-          id: 6,
-          class: "far fa-grin-tongue-squint",
-        },
-        {
-          id: 7,
-          class: "far fa-kiss-wink-heart",
-        },
-        {
-          id: 8,
-          class: "far fa-meh-rolling-eyes",
-        },
-        {
-          id: 9,
-          class: "far fa-sad-cry",
-        },
-        {
-          id: 10,
-          class: "far fa-angry",
-        },
-        {
-          id: 11,
-          class: "far fa-thumbs-up",
-        },
-        {
-          id: 12,
-          class: "far fa-thumbs-down",
-        },
-        {
-          id: 13,
-          class: "far fa-hand-peace",
-        },
-        {
-          id: 14,
-          class: "fas fa-glass-cheers",
-        },
-        {
-          id: 15,
-          class: "fas fa-praying-hands",
-        },
-        {
-          id: 16,
-          class: "fas fa-volume-up",
-        },
-        {
-          id: 17,
-          class: "fas fa-volume-mute",
-        },
-        {
-          id: 18,
-          class: "fas fa-music",
-        },
-        {
-          id: 19,
-          class: "fas fa-backward",
-        },
-        {
-          id: 20,
-          class: "fas fa-forward",
-        },
-        {
-          id: 21,
-          class: "fas fa-random",
-        },
-      ],
+      emojis: null,
       isEmoji: false,
       sentEmojis: [],
     };
@@ -137,6 +53,9 @@ export default {
     toggleEmojiPicker() {
       this.isEmoji = !this.isEmoji;
     },
+  },
+  created() {
+    this.emojis = emojiService.getEmojis();
   },
   sockets: {
     reciveEmoji(emoji) {
