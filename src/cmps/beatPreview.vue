10cb5528d9e8787d66cb30824b0c1e1a29b3b69b
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-12-06 21:56:20 +0200

    ASdas

--- src/cmps/beatPreview.vue
+++ src/cmps/beatPreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div
+  <div v-if="beat"
     class="prev-card flex col justify-center align-center icon"
     @click="beatPrev(beat._id)"
   >
@@ -10,17 +10,20 @@
       </div>
       <div class="prev-details flex col align-center justify-center">
         <h2 class="prev-name">{{ beat.name }}</h2>
-        <h3 class="prev-genre">{{ beat.genre }}</h3>
+        <!-- <h3 class="prev-genre">{{ beat.genre }}</h3> -->
+        <span class="">Now:<br></span><span class="playing-now">{{beat.currSong.title.substring(0,25)}}</span>
+        
       </div>
     </div>
     <div class="prev-info-container flex space-around align-center">
-      <div class="flex">
-        <i class="prev-show prev-show fas fa-headphones-alt"></i>
-        <span class="prev-info">25</span>
-      </div>
+     
       <div class="flex">
         <i class="prev-show fas fa-music"></i>
         <span class="prev-info">{{ beat.songs.length }} </span>
+      </div>
+      <div class="flex tones-prev">
+           <img class="tones-prev1" src=".././assets/img/preview.png" alt="">
+           <img class="tones-prev2" src=".././assets/img/sound-gif2.gif" alt="">
       </div>
       <div class="flex">
         <i class="prev-show prev- fas fa-heart"></i>
@@ -30,6 +33,7 @@
   </div>
 </template>
 <script>
+import MarqueeText from 'vue-marquee-text-component';
 export default {
   props: ["beat"],
   name: "beatPreview",
@@ -38,6 +42,11 @@ export default {
       this.$router.push(`/beat/${beatId}`);
     },
   },
+  components:{
+    MarqueeText
+  }
+
+ 
 };
 </script>
 
